2eb442f8107c7683b08791c0d12bb761eca951f5
mickael.roger <mickael.roger@thalesgroup.com>
2021-03-06 22:09:23 +0100

    packaging

--- setup.py
+++ setup.py
@@ -38,13 +38,17 @@ packages = [
 needs_pytest = set(['pytest', 'test']).intersection(sys.argv)
 pytest_runner = ['pytest_runner'] if needs_pytest else []
 
+test_require = []
+with io.open('requirements-dev.txt') as f:
+    test_require = [l.strip() for l in f if not l.startswith('#')]
+
 install_require = []
 with io.open('requirements.txt') as f:
     install_require = [l.strip() for l in f if not l.startswith('#')]
 
 setup_params = dict(
     name=name,
-    version='FAKE_VERSION',
+    version='1.9.0',
     description=desc,
     long_description=long_desc,
     classifiers=classifiers,
@@ -55,6 +59,7 @@ setup_params = dict(
     packages=packages,
     include_package_data=True,
     install_requires=install_require,
+    tests_require=test_require,
     setup_requires=pytest_runner,
     python_requires='>=3.5.*'
 )
