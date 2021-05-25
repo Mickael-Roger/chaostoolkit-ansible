c44bb256669bc7e96acc2bdc6bc4e4506d4ff736
Mickael-Roger <33000645+Mickael-Roger@users.noreply.github.com>
2021-05-25 21:01:16 +0200

    Issue8 (#12)
    * debug
    
    * debug
    
    * key
    
    * dir
    
    * missing dir
    
    * debug ssh
    
    * ssh debug
    
    * with sudo
    
    * debug mode
    
    * debug auth
    
    * more debug
    
    * password?
    
    * passwd
    
    * restart sshd
    
    * change user
    
    * Try
    
    * global
    
    * global requirements
    
    * Chaos user only
    
    * pylama
    
    * chaos version
    
    * Add build
    
    * pip install
    
    * re requirement-dev from setup
    
    * bad setup conf
    
    * packaging
    
    * chaneg version
    
    * chaos test
    
    * change test
    
    * with become pass
    
    * typo in jsonpath
    
    * Activate Matrix
    
    * ansible skip conflict
    
    * Remove previous ansible installation
    
    * Remove ansible
    
    * Set version
    
    * Add auto release
    
    * Fix become password
    
    * typo
    
    * Fix GitHub runner issue with ssh (#10)
    
    * run github runner with improved error message
    
    * typo
    
    * debug ssh workers
    
    * debug ssh conn
    
    * debug ssh
    
    * debug 3
    
    Co-authored-by: mickael.roger <mickael.roger@thalesgroup.com>
    
    * fix github runner
    
    * Closes #8
    
    Co-authored-by: mickael.roger <mickael.roger@thalesgroup.com>

--- chaosansible/__init__.py
+++ chaosansible/__init__.py
@@ -2,4 +2,38 @@
 
 """Top-level package for chaostoolkit-ansible."""
 
-__version__ = 'IN_PROGESS'
+from typing import List
+
+from chaoslib.discovery.discover import (discover_actions, discover_probes,
+                                         initialize_discovery_result)
+from chaoslib.types import DiscoveredActivities, Discovery
+from logzero import logger
+
+__version__ = 'IN_PROGRESS'
+
+
+def discover(discover_system: bool = True) -> Discovery:
+    """
+    Discover Ansible capabilities from this extension.
+    """
+    logger.info("Discovering capabilities from chaostoolkit-ansible")
+
+    discovery = initialize_discovery_result(
+        "chaostoolkit-ansible", __version__, "ansible")
+    discovery["activities"].extend(load_exported_activities())
+
+    return discovery
+
+
+###############################################################################
+# Private functions
+###############################################################################
+def load_exported_activities() -> List[DiscoveredActivities]:
+    """
+    Extract metadata from actions and probes exposed by this extension.
+    """
+    activities = []
+    activities.extend(discover_probes("chaosansible.probes"))
+    activities.extend(discover_actions("chaosansible.actions"))
+
+    return activities
