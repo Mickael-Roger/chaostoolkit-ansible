a5ab38187b648a2caea21b2c9ec7c050d3bff587
Maxime Loliée <loliee@users.noreply.github.com>
2021-10-04 08:34:44 +0200

    Add ssh extra args opt (#13)
    * Lint files with black & isort
    
    * Add .flake8 files
    
    * Remove useless condition
    
    If key isn't found `None` is return.
    
    * Add ansible_extra_ssh_args option
    
    * Use a real fake address for unreachable test
    
    1.1.1.1 is a real IP owned by Cloudflare…

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,17 +1,17 @@
-import pytest
-
-from chaoslib.exceptions import ActivityFailed, InvalidActivity
-
-from chaosansible.actions import chaosansible_run, random_host
 import json
 
+import pytest
+from chaosansible.actions import chaosansible_run, random_host
+from chaoslib.exceptions import ActivityFailed, InvalidActivity
 
 
 # Test random
 def test_chaosansible_run_random():
-    result = chaosansible_run(ansible={'module':'shell', 'args': 'id'},
-                                host_list=['localhost', '127.0.0.1'],
-                                num_target="1")
+    result = chaosansible_run(
+        ansible={"module": "shell", "args": "id"},
+        host_list=["localhost", "127.0.0.1"],
+        num_target="1",
+    )
 
     jsonresult = json.loads(result)
 
@@ -22,9 +22,10 @@ def test_chaosansible_run_random():
 def test_chaosansible_run_failed():
 
     with pytest.raises(ActivityFailed) as excinfo:
-        chaosansible_run(ansible={'module':'shell', 'args': 'fghf'},
-                                  host_list=['localhost'])
-    
+        chaosansible_run(
+            ansible={"module": "shell", "args": "fghf"}, host_list=["localhost"]
+        )
+
     assert "Failed to run ansible task" in str(excinfo.value)
 
 
@@ -32,9 +33,10 @@ def test_chaosansible_run_failed():
 def test_chaosansible_run_unreachable():
 
     with pytest.raises(ActivityFailed) as excinfo:
-        chaosansible_run(ansible={'module':'shell', 'args': 'ls'},
-                                  host_list=['1.1.1.1'])
-    
+        chaosansible_run(
+            ansible={"module": "shell", "args": "ls"}, host_list=["0.0.0.0"]
+        )
+
     assert "At least one target is down" in str(excinfo.value)
 
 
@@ -42,8 +44,7 @@ def test_chaosansible_run_unreachable():
 def test_chaosansible_run_no_ansible_module():
 
     with pytest.raises(InvalidActivity) as excinfo:
-        chaosansible_run(ansible={'args': 'ls'},
-                                  host_list=['localhost'])
+        chaosansible_run(ansible={"args": "ls"}, host_list=["localhost"])
 
     assert "No ansible module defined" in str(excinfo.value)
 
@@ -52,15 +53,15 @@ def test_chaosansible_run_no_ansible_module():
 def test_chaosansible_run_no_args():
 
     with pytest.raises(InvalidActivity) as excinfo:
-        chaosansible_run(ansible={'module': 'shell'},
-                                  host_list=['localhost'])
+        chaosansible_run(ansible={"module": "shell"}, host_list=["localhost"])
 
     assert "No ansible module args defined" in str(excinfo.value)
+
 
 # Test random number of host
 def test_random_host():
 
-    host_list = ['host1', 'host2', 'host3']
+    host_list = ["host1", "host2", "host3"]
 
     resultone = random_host(host_list, 1)
     resultthree = random_host(host_list, 3)
@@ -70,11 +71,9 @@ def test_random_host():
 
     with pytest.raises(InvalidActivity) as error0:
         random_host(host_list, 0)
-        
+
     with pytest.raises(InvalidActivity) as errorminus:
         random_host(host_list, -1)
-
-
 
     assert len(resultone) == 1 and set(resultone).issubset(host_list)
     assert len(resultthree) == 3 and set(resultthree).issubset(host_list)
@@ -82,4 +81,3 @@ def test_random_host():
     assert "Number of target is not correct" in str(error4.value)
     assert "Number of target is not correct" in str(error0.value)
     assert "Number of target is not correct" in str(errorminus.value)
-
